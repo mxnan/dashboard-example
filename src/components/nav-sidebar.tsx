0c3f338b04bf49f3aa3e44667fec9195ecdcd848
mxnan <128995981+mxnan@users.noreply.github.com>
2024-07-31 17:07:01 +0530

    :hammer:
    :heavy_plus_sign: changed nav-sidebar to sticky

--- src/components/nav-sidebar.tsx
+++ src/components/nav-sidebar.tsx
@@ -30,26 +30,28 @@ export const navLinks: Array<NavGroup> = [
 export default function NavigationSidebar() {
   const pathname = usePathname();
   return (
-    <nav className="max-w-[200px] w-full max-md:hidden pt-4 flex flex-col gap-8">
-      {navLinks.map((group) => (
-        <div key={group.title} className="flex flex-col gap-3">
-          <p className=" font-semibold">{group.title}</p>
-          {group.links.map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className={cn(
-                "ml-3 text-sm font-medium hover:text-gray-500 hover:dark:text-gray-400 transition-colors ease-in-out duration-500",
-                pathname === link.href
-                  ? "text-stone-500 dark:text-stone-400"
-                  : ""
-              )}
-            >
-              {link.title}
-            </Link>
-          ))}
-        </div>
-      ))}
+    <nav className="max-w-[200px] w-full relative max-md:hidden pt-4">
+      <div className="lg:flex flex-1 sticky top-24 flex-col items-start gap-8">
+        {navLinks.map((group) => (
+          <div key={group.title} className="flex flex-col gap-3">
+            <p className=" font-semibold">{group.title}</p>
+            {group.links.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className={cn(
+                  "ml-3 text-sm font-medium hover:text-gray-500 hover:dark:text-gray-400 transition-colors ease-in-out duration-500",
+                  pathname === link.href
+                    ? "text-stone-500 dark:text-stone-400"
+                    : ""
+                )}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        ))}
+      </div>
     </nav>
   );
 }
